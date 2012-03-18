52acd25a33a9b36da39a78ab4f39ca6a261c6ddf
Alex Miller <alexmiller@gmail.com>
2012-03-18 16:43:24 -0700

    deploy script now stops previous process

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
-node web/run.js
+if [ "$1" = "stop" ]; then
+    if [ -f web.pid ]; then
+        kill -INT `cat web.pid`
+        rm web.pid
+        echo "Stopped."
+    else
+        echo "Game not running."
+    fi
+else
+    if [ -f web.pid ]; then
+        kill -INT `cat web.pid`
+    fi
+    node web/run.js >> log.txt & 
+    echo $! > web.pid
+    echo "Started."
+fi
